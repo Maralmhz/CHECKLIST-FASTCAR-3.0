cdaf75f1f2c8f79b30c98d2a3ea649f95caded87
Maralmhz <maralmhz@gmail.com>
2025-12-28 20:14:51 -0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+window.OFICINA_CONFIG = {
+    nome: "NOME DA OFICINA",
+    subtitulo: "CHECKLIST DE ENTRADA E INSPEÇÃO VEICULAR",
+    logo: "logo.png",
+    corPrimaria: "#e41616",
+    endereco: "ENDEREÇO COMPLETO",
+    telefone: "(00) 00000-0000"
+};
